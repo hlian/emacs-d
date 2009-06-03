7b22a9248dd9f0698f366219de507153904b1161
hlian <Me@TORT.(none)>
2009-06-03 18:14:08 -0400

    Feature: white text, pink trailing whitespace.

--- theme.el
+++ theme.el
@@ -36,13 +36,16 @@
 
       ;; http://pocoo.org/~gbrandl/dotemacs
       (custom-set-faces
+       '(default ((t (:stipple nil :background "#102e4e" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :family "outline-consolas"))))
        '(ido-indicator ((((min-colors 88) (class color)) (:background "RoyalBlue1" :width condensed))))
        '(ido-only-match ((((class color)) (:foreground "white"))))
        '(ido-subdir ((((min-colors 88) (class color)) (:foreground "LightPink1"))))
        '(mode-line ((t (:background "#C0F500" :foreground "#666666" :box (:line-width 1 :color "#C0F500") :height 90))))
        '(mode-line-buffer-id ((t (:foreground "black" :weight bold))))
        '(mode-line-highlight ((t (:inherit mode-line :box nil))))
-       '(mode-line-inactive ((t (:background "white" :foreground "#cccccc" :box (:line-width 1 :color "white") :height 90)))))
+       '(mode-line-inactive ((t (:background "white" :foreground "#cccccc" :box (:line-width 1 :color "white") :height 90))))
+       '(trailing-whitespace ((t (:background "LightPink1"))))
+       )
       (set-face-background 'hl-line "black")
       (set-face-background 'mode-line-highlight "#C0F500")
       )
