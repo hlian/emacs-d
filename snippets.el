2a4f99c8dd1e36ac97ffccd031f040b68289a2fd
hlian <Me@TORT.(none)>
2009-06-03 18:46:19 -0400

    Feature: snippets.el.

---
+++ snippets.el
@@ -0,0 +1,17 @@
+(require 'yasnippet)
+(yas/initialize)
+(setq yas/trigger-key (kbd "M-/"))
+(add-to-list 'yas/extra-mode-hooks 'cperl-mode-hook)
+
+(yas/define-snippets
+ 'cperl-mode
+ '(("use" "use strict; use warnings; use 5.010;\n")
+   ("env" "#!/usr/bin/env perl\n")))
+
+(yas/define-snippets
+ 'python-mode
+ '(("env" "#!/usr/bin/env python\n")
+   ("cls" "class ${class}(${object}):$0")
+   ("def" "def ${function}(${args}):$0")
+   ("init" "def __init__(self, ${args}):$0")
+   ))
