0a806459632610f53500e79ab5974e0c53c0d4f2
Hao Lian <me@haolian.org>
2010-07-30 11:04:26 -0400

    Cleanup: no more ising fonts.

--- init-theme.el
+++ init-theme.el
@@ -1,15 +1,3 @@
-;; http://microsoft.com/downloads/details.aspx?FamilyId=941b3470-3ae9-4aee-8f43-c6bb74cd1466
-(if is-sally
-    (custom-set-faces
-       '(default ((t (:stipple nil
-                               :background "white" :foreground "black"
-                               :inverse-video nil :box nil
-                               :strike-through nil :overline nil
-                               :underline nil :slant normal
-                               :weight normal :height 90
-                               :width normal  :family "outline-consolas"))))
-       ))
-
 (defun w32-maximize-frame ()
   "Maximize the current frame"
   (interactive)
