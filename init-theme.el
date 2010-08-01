1b384783cdfec06b07265b69015acc4acf2e0dd3
Hao Lian <me@haolian.org>
2010-07-31 20:34:53 -0400

    Cleanup: remove win32-only init-theme calls.

--- init-theme.el
+++ init-theme.el
@@ -1,8 +1,2 @@
-(defun w32-maximize-frame ()
-  "Maximize the current frame"
-  (interactive)
-  (w32-send-sys-command 61488))
-
-      (add-hook 'window-setup-hook 'w32-maximize-frame)
       (setq-default line-spacing 1)
       (setq-default show-trailing-whitespace t)
