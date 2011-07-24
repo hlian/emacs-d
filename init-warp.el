513e4d19581e0d03a3c5f40a34662e2366c32c54
Hao Lian <me@haolian.org>
2011-07-24 01:54:06 -0400

    Warp mode comments.

--- init-warp.el
+++ init-warp.el
@@ -6,16 +6,16 @@
     (make-directory warp 't)
     (puthash name warp warp-table)))
 
-(new-warp "nims" "pylonshello")
-(new-warp "ptx" "ptx")
-(new-warp "oduslist" "oduslist")
-(new-warp "brownstone" "brownstone")
-(new-warp "brev" "breviloquention")
+(new-warp "nims" "pylonshello") ; Ruby something
+(new-warp "ptx" "ptx") ; Backbone.js
+(new-warp "oduslist" "oduslist") ; This is a promise
+(new-warp "426" "brownstone") ; COS 426
+(new-warp "333" "breviloquention") ; COS 333
 (new-warp "stomp" "stomping-ground")
-(new-warp "dswap" "dswap")
-(new-warp "scg" "scg")
-(new-warp "cf" "cf")
-
-(new-warp "cos318" "cos318")
-(new-warp "iw" "iw")
+(new-warp "dswap" "dswap") ; Point
+(new-warp "320" "scg") ; COS 320
+(new-warp "cf" "cf") ; Courseforge
+(new-warp "http" "http") ; libevent experiments
+(new-warp "nn" "nanowrimo") ; NaNoWriMo
+(new-warp "io" "io") ; Google I/O 2011
 (puthash "home" "~/" warp-table)
