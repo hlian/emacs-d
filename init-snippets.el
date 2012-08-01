04b3461aea4e964abe903ea6dec45f2f5a382ccf
Hao Lian <me@haolian.org>
2012-08-01 13:16:35 -0400

    init-snippets.el: SQL snippets.

--- init-snippets.el
+++ init-snippets.el
@@ -5,6 +5,11 @@
 
 (yas/define-snippets
  'text-mode
+ '(("re" "ALTER DATABASE [$1] SET SINGLE_USER WITH ROLLBACK IMMEDIATE\nALTER DATABASE [$1] MODIFY Name = [$2]\nALTER DATABASE [$2] SET MULTI_USER WITH ROLLBACK IMMEDIATE")
+   ("sext" "EXEC sp_configure 'clr enabled', 1; RECONFIGURE;\ngo\nCREATE ASSEMBLY KilnSqlExtensions FROM 'C:\\code\\kiln\\devel\\website\\SqlExtensions\\bin\\Debug\\SqlExtensions.dll' WITH permission_set = safe;\ngo\nCREATE FUNCTION KilnSplitBigints(@list nvarchar(MAX)) RETURNS table(ix bigint) AS EXTERNAL NAME KilnSqlExtensions.SqlExtensions.KilnSplitBigints;\ngo\nCREATE FUNCTION KilnSplitBinaries(@list nvarchar(MAX)) RETURNS table(ix binary(20)) AS EXTERNAL NAME KilnSqlExtensions.SqlExtensions.KilnSplitBinaries;\ngo")))
+
+(yas/define-snippets
+ 'text-mode
  '(("loc" "# Local Variables:\n#   mode: filladapt\n#   fill-column: 50\n# End:\n")))
 
 (yas/define-snippets
