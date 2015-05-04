6c092fc2cd011378b903833e2f09088ab886029d
Hao Lian <hi@haolian.org>
2015-05-04 14:27:28 -0400

    melpa

--- init-packages.el
+++ init-packages.el
@@ -1,3 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives
+         '("melpa" . "http://melpa-stable.milkbox.net/packages/"))
+(package-initialize)
+
 (require 'use-package)
 (setq use-package-verbose t)
 
