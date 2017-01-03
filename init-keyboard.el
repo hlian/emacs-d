433b966261bfc58af4ad4503cda263b1462fb639
Hao Lian <hi@haolian.org>
2017-01-03 01:26:37 -0500

    this is a brand new day

--- init-keyboard.el
+++ init-keyboard.el
@@ -16,6 +16,18 @@
   (message "fill column set to 50")
   (setq fill-column 50))
 
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2016-12-27"
+  (interactive)
+  (let ((-buf (generate-new-buffer "untitled")))
+    (switch-to-buffer -buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)))
+
 (global-set-keys
  '(("C-<backspace>" backward-kill-word)
 
@@ -27,6 +39,7 @@
    ("C-<f11>" my-fill-column)
    ("C-; C-k" (lambda () (interactive) (kill-buffer nil)))
    ("C-; C-b" electric-buffer-list)
+   ("C-S-j C-S-j" xah-new-empty-buffer)
    ("C-; C-f" projectile-find-file)
    ("C-; C-<return>" unfill-paragraph)
    ("C-; C-r" helm-projectile)
