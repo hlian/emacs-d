cfc1c4cc74e1ad6c7c9e9fefb01172775b09b628
Hao Lian <hi@haolian.org>
2017-06-16 02:35:34 -0400

    modeline

--- lisp/hao-mode.el
+++ lisp/hao-mode.el
@@ -16,6 +16,8 @@
     (require 'key-chord)
     (require 'key-seq)
 
+    (sml/setup)
+
     (key-seq-define-global "jj" 'helm-mini)
     (key-seq-define-global "jf" 'helm-projectile)
     (key-seq-define-global "j," 'helm-find-files)
@@ -32,6 +34,7 @@
     (global-auto-revert-mode t)
     (menu-bar-mode 0)
     (tool-bar-mode -1)
+    (toggle-scroll-bar -1)
     (show-paren-mode t)
     (windmove-default-keybindings)
     (auto-revert-mode 1)
@@ -50,4 +53,14 @@
 
 (provide 'hao-mode)
 
+;;  (custom-set-faces
+;;    ;; custom-set-faces was added by Custom.
+;;    ;; If you edit it by hand, you could mess it up, so be careful.
+;;    ;; Your init file should contain only one such instance.
+;;    ;; If there is more than one, they won't work right.
+;;    '(default ((t (:height 90 :width normal :foundry "apple" :family "Fira Mono"))))
+;;    '(mode-line ((t (:background "#ccffff" :foreground "black" :box (:line-width 1 :color "#ccffff" :style unspecified) :overline "#cfffff" :underline "#cfffff" :height 90))))
+;;    '(mode-line-inactive ((t (:background "#ede6cc" :foreground "#93a1a1" :box (:line-width 1 :color "#fdf6e3" :style unspecified) :overline "#fdf6e3" :underline "#fdf6e3" :height 90))))
+;;    '(sml/filename ((t (:foreground "black" :weight bold))))))
+;; e
 ;;; hao-mode.el ends here
