a46aa72422e70de71717a05c420c731e7dd1e342
Hao Lian <hi@haolian.org>
2019-11-11 13:44:45 -0500

    thank you internet

--- hao/pkgs.el
+++ hao/pkgs.el
@@ -342,6 +342,7 @@
 
 (use-package magit
   :commands (magit magit-process-file)
+  :after evil-magit
   :straight t
   :config
   (require 'evil-magit)
@@ -362,10 +363,29 @@
   :config
   (defun git-commit-turn-on-auto-fill ()))
 
+;; Suppress the message saying that the undo history file was
+;; saved (because this happens every single time you save a file).
+
+(defun radian--undo-tree-suppress-undo-history-saved-message
+    (undo-tree-save-history &rest args)
+  (let ((inhibit-message t))
+    (apply undo-tree-save-history args)))
+
+;; Suppress the message saying that the undo history could not be
+;; loaded because the file changed outside of Emacs.
+
+(defun radian--undo-tree-suppress-buffer-modified-message
+    (undo-tree-load-history &rest args)
+  (let ((inhibit-message t))
+    (apply undo-tree-load-history args)))
+
 (use-package undo-tree
   :commands undo-tree-mode
   :custom
-  (undo-tree-auto-save-history t))
+  (undo-tree-auto-save-history t)
+  :config
+  (advice-add #'undo-tree-load-history :around
+              #'radian--undo-tree-suppress-buffer-modified-message))
 
 (use-package yasnippet
   :straight t
